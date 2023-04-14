c30adbb4f1fdb9993421a2cf22d45bd275f23d26
Aslan <korpeevaslan@gmail.com>
2023-04-14 14:21:27 +0500

    fix:

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -5,5 +5,21 @@ const config: UserConfig = {
     extends: ['@commitlint/config-conventional'],
     helpUrl:
         'https://github.com/conventional-changelog/commitlint/#what-is-commitlint',
+    rules: {
+        'body-case': [2, 'always', 'sentence-case'],
+        'body-max-line-length': [1, 'always', 72],
+        'header-max-length': [1, 'always', 52],
+        'type-case': [2, 'always', 'lower-case'],
+        'type-empty': [2, 'always'] ,
+        'subject-empty': [1, 'always'],
+        "type-enum": [2, "always",
+            ["build", "ci", "docs", "feat", "fix", "perf",
+            "refactor", "revert", "style", "test"]
+        ],
+    },
+
+    prompt: {
+
+    }
 };
 module.exports = config
